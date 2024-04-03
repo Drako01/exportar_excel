da4f591ef55704fbdf0dc02fcc8da98143f04643
Alejandro Daniel Di Stefano <addistefano@live.com.ar>
2024-04-03 20:54:34 -0300

    Update excel.php

--- excel.php
+++ excel.php
@@ -22,9 +22,9 @@ $highestColumn = $sheet->getHighestColumn();
 // Itera sobre cada fila del archivo Excel
 for ($row = 2; $row <= $highestRow; $row++) {
     // Obtiene los datos de cada celda
-    $rowData = $sheet->rangeToArray('A' . $row . ':I' . $row, NULL, TRUE, FALSE);
+    $rowData = $sheet->rangeToArray('A' . $row . ':I' . $row, NULL, TRUE, FALSE); // Esto lee solo 9 columnas
 
-    
+    //$rowData = $sheet->rangeToArray('A' . $row . ':' . $highestColumn . $row, NULL, TRUE, FALSE); => Esto es para que lea la mayor cantidad de columnas posibles
     // Convierte el campo "price" a un número eliminando las comas
     $rowData[0][6] = str_replace(',', '', $rowData[0][6]); // Reemplaza las comas por una cadena vacía
     
